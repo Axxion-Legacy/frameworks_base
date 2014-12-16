900ac808dce5d40020c11d0ba83bc667e605cb00
Lars Greiss <kufikugel@googlemail.com>
2014-12-16 17:25:58 +0000

    Frameworks: add ability to disable bar color in battery saver mode (1/2)
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    
    Change-Id: I7a0059e0b15df43dfd24f6cf874930beb899697e
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3866,6 +3866,13 @@ public final class Settings {
         public static final String LEGACY_MENU_RIGHT_LONG_SHORTCUT_URI = "legacy_menu_right_long_shortcut_uri";     
             
         /**
+         * Whether to enable status and navigation bar color in battery saver mode.
+         *
+         * @hide
+         */
+        public static final String BATTERY_SAVER_MODE_COLOR = "battery_save_mode_color";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
