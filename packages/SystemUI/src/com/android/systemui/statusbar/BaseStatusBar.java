513349ab07aaefedbeec2e4c683b5228d2c01fb3
siracuervo <siratutos@gmail.com>
2015-07-29 19:05:34 -0300

    Revert "Frameworks: Only set package name on user heads ups"
    This reverts commit efff6c1272d11bf2d3baecd96ca5d55e19d7b77c.

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -2273,6 +2273,7 @@ public abstract class BaseStatusBar extends SystemUI implements
         if (isHeadsUpInSnooze()) {
             return false;
         }
+        mHeadsUpPackageName = sbn.getPackageName();
         // some predicates to make the boolean logic legible
         boolean isNoisy = (notification.defaults & Notification.DEFAULT_SOUND) != 0
                 || (notification.defaults & Notification.DEFAULT_VIBRATE) != 0
@@ -2312,10 +2313,6 @@ public abstract class BaseStatusBar extends SystemUI implements
                     && !accessibilityForcesLaunch
                     && !isExpanded
                     && !keyguardIsShowing;
-
-            if (interrupt) {
-                mHeadsUpPackageName = sbn.getPackageName();
-            }
         }
 
         try {
