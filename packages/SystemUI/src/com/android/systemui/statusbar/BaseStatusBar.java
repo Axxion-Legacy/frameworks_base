0f4892583f6af38877480fd20a5dca1bfc0b7738
siracuervo <siratutos@gmail.com>
2015-07-29 18:38:34 -0300

    Revert "[2/2] SlimHeadsUp: Add a global 3-way switch"
    This reverts commit 1f33b64ae31e25d0a2994fa2490a9dbf54dc6579.

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -207,7 +207,6 @@ public abstract class BaseStatusBar extends SystemUI implements
     protected boolean mDisableNotificationAlerts = false;
 
     private int mHeadsUpSnoozeTime;
-    private int mHeadsUpGlobalSwitch;
     private long mHeadsUpSnoozeStartTime;
     protected String mHeadsUpPackageName;
 
@@ -995,12 +994,10 @@ public abstract class BaseStatusBar extends SystemUI implements
             if (isThisASystemPackage(pkg, pmUser)) {
                 headsUpButton.setVisibility(View.GONE);
             } else {
-                boolean isHeadsUpEnabledForPackage =
-                        mNoMan.getHeadsUpNotificationsEnabledForPackage(
-                            pkg, appUidF) != Notification.HEADS_UP_NEVER;
-                headsUpButton.setAlpha(isHeadsUpEnabledForPackage ? 1f : 0.5f);
-                setHeadsUpButtonContentDescription((View) headsUpButton,
-                        isHeadsUpEnabledForPackage);
+                boolean isHeadsUpEnabled = mNoMan.getHeadsUpNotificationsEnabledForPackage(
+                        pkg, appUidF) != Notification.HEADS_UP_NEVER;
+                headsUpButton.setAlpha(isHeadsUpEnabled ? 1f : 0.5f);
+                setHeadsUpButtonContentDescription((View) headsUpButton, isHeadsUpEnabled);
                 headsUpButton.setVisibility(View.VISIBLE);
                 headsUpButton.setOnClickListener(new View.OnClickListener() {
                     public void onClick(View v) {
@@ -2318,18 +2315,6 @@ public abstract class BaseStatusBar extends SystemUI implements
                 mHeadsUpSnoozeTime / 60 / 1000), Toast.LENGTH_LONG).show();
     }
 
-    protected boolean isHeadsUpDisabled() {
-        return mHeadsUpGlobalSwitch == 0;
-    }
-
-    protected boolean isHeadsUpForced() {
-        return mHeadsUpGlobalSwitch == 2;
-    }
-
-    protected void setHeadsUpGlobalSwitch(int headsUpGlobalSwitch) {
-        mHeadsUpGlobalSwitch = headsUpGlobalSwitch;
-    }
-
     protected boolean isHeadsUpInSnooze() {
         return (mHeadsUpSnoozeStartTime + mHeadsUpSnoozeTime - System.currentTimeMillis()) > 0;
     }
@@ -2361,22 +2346,15 @@ public abstract class BaseStatusBar extends SystemUI implements
         }
 
         String pkg = sbn.getPackageName();
-
-        // Stop here if :
-        //      headsup is globally disabled or we are globally snoozing
-        //  and
-        //      notification is not a call (intrusive/non-intrusive is handled elsewhere)
-        if ((isHeadsUpDisabled() || isHeadsUpInSnooze()) && !isIncomingCall(pkg)) {
-            return false;
-        }
-
-        // Stop here if headsup is not globally forced and app is snoozed
-        if (!isHeadsUpForced() &&
-                mHeadsUpNotificationView.isSnoozed(pkg)) {
+        if (mHeadsUpNotificationView.isSnoozed(pkg)) {
             return false;
         }
 
         Notification notification = sbn.getNotification();
+        // we are snoozing
+        if (isHeadsUpInSnooze()) {
+            return false;
+        }
         // some predicates to make the boolean logic legible
         boolean isNoisy = (notification.defaults & Notification.DEFAULT_SOUND) != 0
                 || (notification.defaults & Notification.DEFAULT_VIBRATE) != 0
@@ -2411,8 +2389,7 @@ public abstract class BaseStatusBar extends SystemUI implements
         if (DEBUG) Log.d(TAG, "interrupt: "+interrupt);
 
         if (!interrupt) {
-            boolean isHeadsUpPackage = isHeadsUpForced() ||
-                    (mNoMan.getHeadsUpNotificationsEnabledForPackage(
+            boolean isHeadsUpPackage = (mNoMan.getHeadsUpNotificationsEnabledForPackage(
                     pkg, sbn.getUid()) != Notification.HEADS_UP_NEVER);
             if (DEBUG) Log.d(TAG, "package: "+pkg+", isHeadsUpPackage: "+isHeadsUpPackage);
 
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -486,9 +486,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     Settings.System.HEADS_UP_SNOOZE_TIME),
                     false, this, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.HEADS_UP_GLOBAL_SWITCH),
-                    false, this, UserHandle.USER_ALL);
-            resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.PIE_CONTROLS), false, this,
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
@@ -539,14 +536,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                         R.integer.heads_up_notification_decay),
                         UserHandle.USER_CURRENT);
                 resetHeadsUpDecayTimer();
-            } else if (uri.equals(Settings.System.getUriFor(
-                    Settings.System.HEADS_UP_GLOBAL_SWITCH))) {
-                final int headsUpGlobalSwitch = Settings.System.getIntForUser(
-                        mContext.getContentResolver(),
-                        Settings.System.HEADS_UP_GLOBAL_SWITCH,
-                        1,
-                        UserHandle.USER_CURRENT);
-                setHeadsUpGlobalSwitch(headsUpGlobalSwitch);
             } else if (uri.equals(Settings.System.getUriFor(
                     Settings.System.PIE_CONTROLS))) {
                 attachPieContainer(isPieEnabled());
@@ -941,12 +930,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         mHeadsUpNotificationView.setVisibility(View.GONE);
         mHeadsUpNotificationView.setBar(this);
-        final int headsUpGlobalSwitch = Settings.System.getIntForUser(
-                mContext.getContentResolver(),
-                Settings.System.HEADS_UP_GLOBAL_SWITCH,
-                1,
-                UserHandle.USER_CURRENT);
-        setHeadsUpGlobalSwitch(headsUpGlobalSwitch);
         mHeadsUpNotificationDecay = Settings.System.getIntForUser(
                 mContext.getContentResolver(),
                 Settings.System.HEADS_UP_NOTIFCATION_DECAY,
